cc52deac357d5990672c2744a94bbdc32092ddee
Jaffar Mallem <jmtrading@live.com>
2019-06-04 17:43:55 +0200

    Creation vue de Nos bons plans

---
+++ src/Repository/GoodDealRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\GoodDeal;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method GoodDeal|null find($id, $lockMode = null, $lockVersion = null)
+ * @method GoodDeal|null findOneBy(array $criteria, array $orderBy = null)
+ * @method GoodDeal[]    findAll()
+ * @method GoodDeal[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class GoodDealRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, GoodDeal::class);
+    }
+
+    // /**
+    //  * @return GoodDeal[] Returns an array of GoodDeal objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('g.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?GoodDeal
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
