8e8d8c5a2c3c26e505049a9cccc0aca6488b1dba
Manuel <manu.wiizzle@gmail.com>
2019-06-06 17:56:54 +0200

    map sur homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,7 @@
 const $ = require('jquery');
 require('../scss/app.scss');
 require('bootstrap');
+require('leaflet');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 
 
