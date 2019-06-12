abc544fb54aa34c2f26eaf00eee41cefee624134
Angel <a.rioncervi@gmail.com>
2019-06-12 15:10:05 +0200

    removed dep

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,6 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
-;
+;erge
 
 module.exports = Encore.getWebpackConfig();
