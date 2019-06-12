893470537ca70c0c32023c45f7ff81315a7f955e
Angel <a.rioncervi@gmail.com>
2019-06-12 14:05:23 +0200

    rfzsvqz

--- src/Controller/MonProjetController.php
+++ src/Controller/MonProjetController.php
@@ -29,55 +29,6 @@ class MonProjetController extends AbstractController
 
     public function index(Request $request) :Response
     {
-        // creates a task and gives it some dummy data for this example
-        $task = new MainWorkCategories();
-        $form = $this->createFormBuilder($task)
-            ->add('Couverture', CheckboxType::class, ['label' => 'Couverture', 'required' => ''])
-            ->add('Isolation', CheckboxType::class, ['label' => 'Isolation', 'required' => ''])
-            ->add('Interieur', CheckboxType::class, ['label' => 'Interieur', 'required' => ''])
-            ->add('Ravalement', CheckboxType::class, ['label' => 'Ravalement', 'required' => ''])
-            ->add('Menuiserie', CheckboxType::class, ['label' => 'Menuiserie', 'required' => ''])
-            ->add('Terrassement', CheckboxType::class, ['label' => 'Terrassement', 'required' => ''])
-            ->add('save', SubmitType::class, ['label' => 'Filter'])
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $task = $form->getData();
-
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($task);
-            // $entityManager->flush();
-            var_dump($task);
-        }
-
-        return $this->render('monProjet/index.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render('monProjet/index.html.twig');
     }
-
-    /**
-     * @Route("/projetForm", name="projet2")
-     */
-
-    public function create(Request $request)
-    {
-        $mainWork = new MainWorkCategories();
-        $form = $this->createForm(MainWorkCategories::class, $mainWork);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // ... save the meetup, redirect etc.
-        }
-
-        return $this->render(
-            'monProjet/index.html.twig',
-            ['form' => $form->createView()]
-        );
-    }
-
 }
