9f7420937e7b6afd8147d08bd56f878c7a8a5c5f
Angel <a.rioncervi@gmail.com>
2019-06-13 18:16:12 +0200

    update dev

--- assets/js/mappedImage.js
+++ assets/js/mappedImage.js
@@ -2,15 +2,12 @@
 
 $(document).ready(function() {
 
-    $(window).resize()
-
     $('.tooltip').tooltipster({
         animation: "fade",
         delay: 200,
         theme: ".tooltipster-default",
         position: "top"
     });
-
     $('.tooltip').tooltipster('open')
     $('img[usemap]').rwdImageMaps();
 });
