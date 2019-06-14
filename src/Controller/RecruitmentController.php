2a61f5d0c50cf2f23d8a51617b15290597f3046b
Jaffar Mallem <jmtrading@live.com>
2019-06-14 12:02:56 +0200

    Recrutement formulaire initial

--- src/Controller/RecruitmentController.php
+++ src/Controller/RecruitmentController.php
@@ -2,13 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Recruitment;
 use App\Form\RecruitmentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 /**
  * @Route("/recruitment")
@@ -19,16 +17,29 @@ class RecruitmentController extends AbstractController
     /**
      * @Route("/", name="recruitment", methods={"GET","POST"})
      */
-    public function show(Request $request): Response
+    public function show(Request $request, \Swift_Mailer $mailer): Response
     {
-        $recruitment = new Recruitment();
-        $form = $this->createForm(RecruitmentType::class, $recruitment);
+        $form = $this->createForm(RecruitmentType::class);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $message = (new \Swift_Message('Nouvelle demande de recruitment !'))
+                ->setFrom($this->getParameter('mailer_from'))
+                ->setTo($this->getParameter('mailer_from'))
+                ->setBody('Nouvelle demande de recruitment !
+                 Nom:' . $data['lastname'] . ' ' . 'Prénom:' . $data['firstname']
+                    . ' ' . 'Métier:' . $data['occupation'] . ' ' . 'Votre CV:' . ' ' . $data['cv']);
+            $mailer->send($message);
+
+            $this->addFlash('success-message',
+                'Votre message a bien été envoyé. Nous vous contacterons bientot, merci.');
+            return $this->redirectToRoute('recruitment');
+        }
+
         return $this->render('recruitment/show.html.twig', [
-            'recruitment' => $recruitment,
             'form' => $form->createView()
         ]);
     }
-
 }
