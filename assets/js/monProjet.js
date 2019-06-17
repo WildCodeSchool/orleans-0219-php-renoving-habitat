4d47b793182d1573d37340b73a044bb585cfc26c
Angel <a.rioncervi@gmail.com>
2019-06-17 17:04:27 +0200

    all forms data accessed

--- assets/js/monProjet.js
+++ assets/js/monProjet.js
@@ -1,11 +1,9 @@
-
-
 $(document).ready(() => {
 
     // uncheck all checkbox on page load
     let checkboxes = document.getElementsByTagName('input');
-    for(var i = 0; i <checkboxes.length; i++){
-        if(checkboxes[i].type == 'checkbox'){
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].type == 'checkbox') {
             checkboxes[i].checked = false;
         }
     }
@@ -20,58 +18,58 @@ $(document).ready(() => {
     ]
 
 
-
     let elementsArray = document.getElementsByClassName("form-check-input");
 
-    elementsArray.forEach(function(elem, index) {
-        elem.addEventListener("change", function() {
+    elementsArray.forEach(function (elem, index) {
+        elem.addEventListener("change", function () {
             controlForm(elem, index)
         });
     });
 
 
     function showElement(index) {
-        elementsArray.forEach(function(elem, i) {
-            if (index===i) {
+        elementsArray.forEach(function (elem, i) {
+            if (index === i) {
                 forms[i].style.display = 'block';
             }
         });
     }
 
     function hideElement(index) {
-        elementsArray.forEach(function(elem, i) {
-            if (index===i) {
+        elementsArray.forEach(function (elem, i) {
+            if (index === i) {
                 forms[i].style.display = 'none';
             }
         });
     }
 
-    function controlForm (elem, index) {
+    function controlForm(elem, index) {
         if (elem.checked) {
             showElement(index)
         } else {
-        hideElement(index);
+            hideElement(index);
         }
     }
 
-    document.getElementById('sendAllForms').addEventListener('click', ()=>{
+    document.getElementById('sendAllForms').addEventListener('click', () => {
+        getFormData()
+    })
 
-        let persInfo = {};
+    function getFormData() {
+        let infos = {personal:{}};
 
         let persInfoForms = document.getElementsByClassName('info_pers_form');
 
         persInfoForms.forEach((e) => {
-            persInfo[e.children[0].innerText] = e.children[1].value;
+            infos.personal[e.children[0].innerText] = e.children[1].value;
         });
-        console.log(persInfo);
 
-        forms.forEach((v)=>{
+        forms.forEach((v) => {
             if (v.style.display === 'block') {
-                let formInputs = document.querySelectorAll('#'+ v.id +' .list-group-item');
+                let formInputs = document.querySelectorAll('#' + v.id + ' .list-group-item');
                 let formInfo = {};
-                formInputs.forEach((e)=>{
-
-                    if (e.children[0].nodeName !== 'DIV') {
+                formInputs.forEach((e) => {
+                    if (e.children[0].nodeName !== 'DIV') { //dealing with checkbox
                         formInfo[e.innerText] = e.children[0].children[0].checked
                     } else { //dealing with radio
                         let choices = Array.from(e.children[0].children);
@@ -80,9 +78,9 @@ $(document).ready(() => {
                         formInfo[key] = chosen[0].innerText;
                     }
                 })
-                persInfo[v.id.split(/(?=[A-Z])/).pop()] = formInfo;
+                infos[v.id.split(/(?=[A-Z])/).pop()] = formInfo;
             }
         })
-        console.log(persInfo)
-    })
+        console.log(infos)
+    }
 })
