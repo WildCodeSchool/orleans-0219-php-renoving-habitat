a1f2ce46807e10a7013bcc752e00f780941e12d1
Angel <a.rioncervi@gmail.com>
2019-06-24 10:11:36 +0200

    first commit, controller

---
+++ src/Controller/NosRealisationsController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: angel
+ * Date: 03/06/19
+ * Time: 10:55
+ */
+
+namespace App\Controller;
+
+use App\Entity\GoodDeal;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NosRealisationsController extends AbstractController
+{
+
+    /**
+     * @Route("/realisations", name="nos_real")
+     * @return Response
+     */
+
+    public function index(): Response
+    {
+        return $this->render('nos_realisations/index.html.twig');
+    }
+}
