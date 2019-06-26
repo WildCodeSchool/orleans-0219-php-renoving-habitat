3db75344c415126b6ed22727123c7e10a56620bc
Vanina Diot <vanina.diot@gmail.com>
2019-06-26 23:40:34 +0200

    Init Admin

---
+++ src/Entity/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AdminRepository")
+ */
+class Admin
+{
+
+}
