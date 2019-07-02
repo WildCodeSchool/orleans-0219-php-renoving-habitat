fa7de450f04c95fed84c8d2e9e1aa1397682a0c4
Vanina Diot <vanina.diot@gmail.com>
2019-07-02 11:22:29 +0200

    finissions notre entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
-class EntrepriseController  extends AbstractController
+class EntrepriseController extends AbstractController
 {
     /**
      * @Route("/entreprise", name="entreprise")
