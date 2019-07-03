8346179a2388ef14093b257558fcbed5a90f245b
Angel <a.rioncervi@gmail.com>
2019-07-03 15:13:27 +0200

    renamed route

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-            $this->addFlash('success', 'Bravo ! Voous pouvez des à présent vous connecté !');
+            $this->addFlash('success', 'Bravo ! Vous pouvez des à présent vous connecté !');
             return $this->redirectToRoute('app_login');
         }
         return $this->render('registration/register.html.twig', [
