834fa62cbd9ae82ad3a545eff195afae6155959d
Vanina Diot <vanina.diot@gmail.com>
2019-07-22 12:23:53 +0200

    ©≈≈©©≈©

--- src/Migrations/Version20190709084416.php
+++ src/Migrations/Version20190709084416.php
@@ -21,12 +21,11 @@ final class Version20190709084416 extends AbstractMigration
     {
         $this->addSql("INSERT INTO `customer_review` VALUES (5,'Sylvain','Blondeau','J\'adore le tarvail effectué par l\'équipe, merci !')");
         $this->addSql("INSERT INTO `good_deal` VALUES (2,'-20% sur la menuiserie','Pour cette fin d\'année TVA OFFERTES pour tout travaux d\'isolation thermique et nettoyage toiture 9.99€/m2');");
-        $this->addSql('INSERT INTO `user` VALUES (5,\'renoving3a@gmail.com\',\'[\"ROLE_ADMIN\"]\',\'$argon2i$v=19$m=1024,t=2,p=2$TzU1R2dISDk2UFdLeVgzNg$pJXVrEOzl7pT6UJ0tnsqVugjcKOnMBuCpNix/9LQlD4\',\'toitureetfacade\');');
+        $this->addSql('INSERT INTO `user` VALUES (5,\'toitureetfacade\',\'[\"ROLE_ADMIN\"]\',\'$argon2i$v=19$m=1024,t=2,p=2$TzU1R2dISDk2UFdLeVgzNg$pJXVrEOzl7pT6UJ0tnsqVugjcKOnMBuCpNix/9LQlD4\',\'renoving3a@gmail.com\');');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('TRUNCATE TABLE user; TRUNCATE TABLE customer_review; TRUNCATE TABLE good_deal;');
-
     }
 }
